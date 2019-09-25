7b35a2a9dcea65f83798c49c4ddea9240372e3c2
Rishabh Jain <rishabh.jain@walmartlabs.com>
2019-09-25 22:48:01 +0530

    test hook

--- django_test/views.py
+++ django_test/views.py
@@ -1,6 +1,10 @@
 from django.http import JsonResponse
+import git
+import os
 
+# g = git.cmd.Git(git_dir)
+# g.pull()
 
 def testing(req):
     print(req)
-    return JsonResponse({'first': 'Rishabh', 'last': 'Jain', 'age': 24 })
+    return JsonResponse({'first': 'Rishabh', 'last': 'Jain', 'age': 24, 'path': os.listdir('')})
